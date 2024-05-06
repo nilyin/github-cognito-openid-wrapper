d6b501ace95d8b1a491333232ce781878c629b88
Pavel Tcholakov <pavel@restate.dev>
2024-05-06 14:04:28 +0200

    Reset prettierrc

--- cdk-app.ts
+++ cdk-app.ts
@@ -1,8 +1,8 @@
-import * as cdk from "aws-cdk-lib";
-import * as apigateway from "aws-cdk-lib/aws-apigateway";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as secretsmanager from "aws-cdk-lib/aws-secretsmanager";
-import { Construct } from "constructs";
+import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import { Construct } from 'constructs';
 
 interface GitHubCognitoOpenIdWrapperProps extends cdk.StackProps {
   githubClientId: string;
@@ -16,71 +16,83 @@ interface GitHubCognitoOpenIdWrapperProps extends cdk.StackProps {
 }
 
 export class GitHubCognitoOpenIdWrapper extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: GitHubCognitoOpenIdWrapperProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: GitHubCognitoOpenIdWrapperProps
+  ) {
     super(scope, id, props);
 
-    const githubAppClientSecret = secretsmanager.Secret.fromSecretAttributes(this, "githubAppClientSecret", {
-      secretCompleteArn: props.githubClientSecretArn,
-    });
+    const githubAppClientSecret = secretsmanager.Secret.fromSecretAttributes(
+      this,
+      'githubAppClientSecret',
+      {
+        secretCompleteArn: props.githubClientSecretArn,
+      }
+    );
     const environment = {
       GITHUB_CLIENT_ID: props.githubClientId,
       GITHUB_CLIENT_SECRET: githubAppClientSecret.secretValue.unsafeUnwrap(),
       COGNITO_REDIRECT_URI: props.cognitoRedirectUri,
-      GITHUB_API_URL: props.githubApiUrl ?? "https://api.github.com",
-      GITHUB_LOGIN_URL: props.githubLoginUrl ?? "https://github.com",
+      GITHUB_API_URL: props.githubApiUrl ?? 'https://api.github.com',
+      GITHUB_LOGIN_URL: props.githubLoginUrl ?? 'https://github.com',
     };
-    const handlerCode = lambda.Code.fromAsset("./dist-lambda");
+    const handlerCode = lambda.Code.fromAsset('./dist-lambda');
     const lambdaCommonProps = {
       runtime: lambda.Runtime.NODEJS_18_X,
       code: handlerCode,
       environment,
     };
 
-    const openIdDiscovery = new lambda.Function(this, "OpenIdDiscovery", {
+    const openIdDiscovery = new lambda.Function(this, 'OpenIdDiscovery', {
       ...lambdaCommonProps,
-      handler: "openIdConfiguration.handler",
+      handler: 'openIdConfiguration.handler',
     });
-    const authorize = new lambda.Function(this, "Authorize", {
+    const authorize = new lambda.Function(this, 'Authorize', {
       ...lambdaCommonProps,
-      handler: "authorize.handler",
+      handler: 'authorize.handler',
     });
-    const token = new lambda.Function(this, "Token", {
+    const token = new lambda.Function(this, 'Token', {
       ...lambdaCommonProps,
-      handler: "token.handler",
+      handler: 'token.handler',
     });
-    const userInfo = new lambda.Function(this, "UserInfo", {
+    const userInfo = new lambda.Function(this, 'UserInfo', {
       ...lambdaCommonProps,
-      handler: "userinfo.handler",
+      handler: 'userinfo.handler',
     });
-    const jwks = new lambda.Function(this, "Jwks", {
+    const jwks = new lambda.Function(this, 'Jwks', {
       ...lambdaCommonProps,
-      handler: "jwks.handler",
+      handler: 'jwks.handler',
     });
 
-    const api = new apigateway.RestApi(this, "GithubOAuthApi", {
-      restApiName: "Github Cognito OpenID Wrapper",
+    const api = new apigateway.RestApi(this, 'GithubOAuthApi', {
+      restApiName: 'Github Cognito OpenID Wrapper',
     });
 
-    const wellKnownResource = api.root.addResource(".well-known");
+    const wellKnownResource = api.root.addResource('.well-known');
     wellKnownResource
-      .addResource("openid-configuration")
-      .addMethod("GET", new apigateway.LambdaIntegration(openIdDiscovery));
-    wellKnownResource.addResource("jwks.json").addMethod("GET", new apigateway.LambdaIntegration(jwks));
+      .addResource('openid-configuration')
+      .addMethod('GET', new apigateway.LambdaIntegration(openIdDiscovery));
+    wellKnownResource
+      .addResource('jwks.json')
+      .addMethod('GET', new apigateway.LambdaIntegration(jwks));
 
-    api.root.addResource("authorize").addMethod("GET", new apigateway.LambdaIntegration(authorize));
+    api.root
+      .addResource('authorize')
+      .addMethod('GET', new apigateway.LambdaIntegration(authorize));
 
     const tokenIntegration = new apigateway.LambdaIntegration(token);
-    const tokenResource = api.root.addResource("token");
-    tokenResource.addMethod("GET", tokenIntegration);
-    tokenResource.addMethod("POST", tokenIntegration);
+    const tokenResource = api.root.addResource('token');
+    tokenResource.addMethod('GET', tokenIntegration);
+    tokenResource.addMethod('POST', tokenIntegration);
 
-    const userInfoResource = api.root.addResource("userinfo");
+    const userInfoResource = api.root.addResource('userinfo');
     const userInfoIntegration = new apigateway.LambdaIntegration(userInfo);
-    userInfoResource.addMethod("GET", userInfoIntegration);
-    userInfoResource.addMethod("POST", userInfoIntegration);
+    userInfoResource.addMethod('GET', userInfoIntegration);
+    userInfoResource.addMethod('POST', userInfoIntegration);
 
-    new cdk.CfnOutput(this, "GitHubShimIssuer", {
-      description: "GitHub OpenID Shim Issuer",
+    new cdk.CfnOutput(this, 'GitHubShimIssuer', {
+      description: 'GitHub OpenID Shim Issuer',
       // value: `https://${api.restApiId}.execute-api.${this.region}.amazonaws.com/${api.deploymentStage.stageName}`,
       value: api.url,
     });
@@ -88,10 +100,10 @@ export class GitHubCognitoOpenIdWrapper extends cdk.Stack {
 }
 
 const app = new cdk.App();
-new GitHubCognitoOpenIdWrapper(app, "GithubCognitoOpenidWrapperStack", {
-  githubClientId: app.node.getContext("githubClientId"),
-  githubClientSecretArn: app.node.getContext("githubClientSecretArn"),
-  cognitoRedirectUri: app.node.getContext("cognitoRedirectUri"),
+new GitHubCognitoOpenIdWrapper(app, 'GithubCognitoOpenidWrapperStack', {
+  githubClientId: app.node.getContext('githubClientId'),
+  githubClientSecretArn: app.node.getContext('githubClientSecretArn'),
+  cognitoRedirectUri: app.node.getContext('cognitoRedirectUri'),
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 app.synth();
--- src/connectors/lambda/jwks.js
+++ src/connectors/lambda/jwks.js
@@ -1,6 +1,6 @@
-const responder = require("./util/responder");
-const controllers = require("../controllers");
-const logger = require("../logger");
+const responder = require('./util/responder');
+const controllers = require('../controllers');
+const logger = require('../logger');
 
 module.exports.handler = (event, context, callback) => {
   logger.defaultMeta = { requestId: context.awsRequestId };
